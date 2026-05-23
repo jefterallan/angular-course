aaf896f5700aa713002a446a328989fbea7597c8
Jefter Allan <jefterallan@hotmail.com>
2026-05-23 12:51:05 -0300

    end of section 4

---
+++ DatingApp/API/Extensions/UserDtoExtensions.cs
@@ -0,0 +1,19 @@
+using API.DTOs;
+using API.Entities;
+using API.Interfaces;
+
+namespace API.Extensions;
+
+public static class UserDtoExtensions
+{
+    public static UserDto AsUserDto(this AppUser user, ITokenService tokenService)
+    {
+        return new UserDto
+        {
+            Id = user.Id,
+            Email = user.Email,
+            DisplayName = user.DisplayName,
+            Token = tokenService.CreateToken(user)
+        };
+    }
+}
