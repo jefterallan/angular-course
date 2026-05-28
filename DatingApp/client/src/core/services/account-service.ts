04996e4a674d5963c530740b27aeac92b9dd38c4
Jefter Allan <jefterallan@hotmail.com>
2026-05-28 12:10:58 -0300

    end of section 6

--- DatingApp/client/src/core/services/account-service.ts
+++ DatingApp/client/src/core/services/account-service.ts
@@ -8,7 +8,7 @@ import { tap } from 'rxjs';
 })
 export class AccountService {
     private http = inject(HttpClient);
-    currenUser = signal<User | null>(null);
+    currentUser = signal<User | null>(null);
 
     baseUrl = 'https://localhost:5001/api/';
 
@@ -34,11 +34,11 @@ export class AccountService {
 
     setCurrentUser(user: User) {
       localStorage.setItem('user', JSON.stringify(user));
-      this.currenUser.set(user);
+      this.currentUser.set(user);
     }
 
     logout() {
       localStorage.removeItem('user');
-      this.currenUser.set(null);
+      this.currentUser.set(null);
     }
 }
