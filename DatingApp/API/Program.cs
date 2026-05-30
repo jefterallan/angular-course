9623e9f52f3053f7f02846378aad9c5a224df42b
Jefter Allan <jefterallan@hotmail.com>
2026-05-30 11:05:42 -0300

    end of section 7

--- DatingApp/API/Program.cs
+++ DatingApp/API/Program.cs
@@ -1,5 +1,6 @@
 using API.Data;
 using API.Interfaces;
+using API.Middleware;
 using API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
@@ -33,6 +34,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
     .WithOrigins("https://localhost:4200", "http://localhost:4200"));
 
